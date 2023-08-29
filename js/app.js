5b6d60867cc476803e2052add2305a4cecd1dc60
Leonel Sanagua <leonel.sanagua@gmail.com>
2023-08-29 14:38:08 -0300

    Logueo y deslogueo del usuario

--- js/app.js
+++ js/app.js
@@ -1,4 +1,33 @@
-if (localStorage.getItem("auth")) {
+//DECLARO VARIABLES
+
+let auth = JSON.parse(localStorage.getItem("auth")) || null,
+useravatar = document.getElementById('user-avatar'),
+closeuser = document.getElementById('close-user'),
+avatar = document.getElementById('avatar')
+
+
+//SI EL USUARIO ESTA LOGUEADO OCULTO EL PAGE LOGIN Y MUESTRO AVATAR Y BOTON CERRAR SESION
+
+if (auth) {
   login.classList="d-none"
+  avatar.src = auth.avatar
 }
-else{}
+else{
+  useravatar.classList="d-none"
+  closeuser.classList= "d-none"
+}
+
+//DECLARO FUNCION PARA CERRAR SESION
+closeuser.addEventListener("click", () => {
+  closesesion();
+});
+
+const closesesion =() =>{
+auth = null
+localStorage.removeItem("auth")
+location.reload()
+}
+
+
+
+
