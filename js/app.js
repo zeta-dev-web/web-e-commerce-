aa1eadc0c6ef7651a2f5afdc27b955c43f8e72e7
Franco Garcia <garciafranco_01@live.com>
2023-08-29 23:38:18 -0300

    Crud completado falta conectar con carousel y que sea responsive

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 class Producto {
-    constructor(nombre,precio,categoria,imagen,desc,stock){
+    constructor(nombre,precio,marca,imagen,descripcion,stock){
         this.id = Producto.getNextId()
         this.nombre=nombre
         this.precio=precio
-        this.categoria=categoria
+        this.marca=marca
         this.imagen=imagen
-        this.descripcion=desc
+        this.descripcion=descripcion
         this.stock=stock
     }
     static getNextId() {
@@ -17,116 +17,210 @@ class Producto {
 }
 
 
-let productos=[]
+const productos=JSON.parse(localStorage.getItem("productos")) ||[]
 
 
-let producto1= new Producto("ss",45000,"saas","https://techterms.com/img/xl/laptop_586.png","rwrwrw",15)
-let producto2= new Producto("ss",45000,"saas","https://consumer.huawei.com/content/dam/huawei-cbg-site/latam/mx/support/laptop-user-guide/img/laptop.png","rwrwrw",25)
-let producto3= new Producto("ss",45000,"saas","https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/g-series/g16-7630/media-gallery/black/notebook-g16-7630-nt-black-gallery-1.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=536&qlt=100,1&resMode=sharp2&size=536,402&chrss=full","rwrwrw",5)
-let producto4= new Producto("ss",45000,"saas","https://images.ctfassets.net/aekk6nx6e23n/OceccWc3Xzw4zrVghTzk2/5fd4b0e114949c898d5f21aa8cfd0a72/DRX_-_overview.png","rwrwrw",9)
-let producto5= new Producto("ss",45000,"saas","https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-silver-select-202206?wid=1280&hei=1190&fmt=png-alpha&.v=1664497095569","rwrwrw",8)
+let form= document.querySelector("#myForm"),
+imgurl=document.querySelector("#imgurl"),
+productName=document.querySelector("#name"),
+brand=document.querySelector("#brand"),
+price=document.querySelector("#price"),
+desc=document.querySelector("#desc"),
+stock=document.querySelector("#stock"),
+submitbtn=document.querySelector(".submit"),
+modal=document.querySelector("#userForm"),
+modalTitle=document.querySelector("#userForm .modal-title"),
+userInfo=document.querySelector("#data")
 
 
-productos.push(producto1)
-productos.push(producto2)
-productos.push(producto3)
-productos.push(producto4)
-productos.push(producto5)
+let isEdit=false,editId
 
-let cr1 = document.querySelector(".cr1")
-let cr2 = document.querySelector(".cr2")
-const cargarCar= ()=>{
-cr1.innerHTML=""
-cr2.innerHTML=""
-vendidas=productos.filter((producto)=>producto.stock<10)
-buscadas=productos.filter((producto)=>producto.stock>10)
-vendidas.map((producto,index)=>{
-   
-    let carItem1=document.createElement("div")
-    if (index==0) {
-        carItem1.classList="carousel-item active  " 
-    }else{
-        carItem1.classList="carousel-item " 
-    }
-   let minicar1= document.createElement("div")
-   minicar1.classList="mini-carousel"
-   let imagen1=document.createElement("img")
-   imagen1.src=producto.imagen
-   imagen1.classList="d-block w-100"
-   imagen1.alt=producto.nombre
-   let cardbody=document.createElement("div")
-   cardbody.classList="card-body d-flex flex-column justify-content-between"
-let text1=document.createElement("div")
-text1.classList="text-center px-5 my-2"
-let titu1=document.createElement("h1")
-titu1.classList="badge text-bg-warning text-white fs-5"
-titu1.innerText="Las más vendidas"
-let parr1=document.createElement("p")
-parr1.classList="text-center"
-parr1.innerText="Estas son las notebook más vendidas en el mercado, con los mejores componentes"
-let boton1=document.createElement("div")
-boton1.classList="mt-auto text-center"
-let ancla1=document.createElement("a")
-ancla1.classList="btn btn-warning text-white fs-5 mb-3"
-ancla1.innerText="Más información"
+const showInfo= ()=>{
+    document.querySelectorAll(".detallesProd").forEach(info=>info.remove())
+    productos.forEach((producto,index)=>{
+        let createElement=`<tr class="detallesProd">
+        <td>${producto.id}</td>
+        <td><img src="${producto.imagen}" alt="" width="50" height="50"></td>
+        <td>${producto.nombre}</td>
+        <td>${producto.marca}</td>    
+        <td>${producto.precio}</td>   
+        <td>${producto.descripcion}</td>
+        <td>${producto.stock}</td>
+        <td>
+        <button class="btn btn-success"  data-bs-toggle="modal" data-bs-target="#readData" onclick="readInfo('${producto.imagen}', '${producto.nombre}', '${producto.marca}', '${producto.precio}', '${producto.descripcion}', '${producto.stock}')">
+          <i class="fa fa-eye" aria-hidden="true"></i>
+        </button>
+        <button class="btn btn-primary" onclick="editInfo(${index},'${producto.imagen}','${producto.nombre}','${producto.marca}','${producto.precio}','${producto.descripcion}','${producto.stock}')" data-bs-toggle="modal" data-bs-target="#userForm">
+          <i class="fa fa-pencil" aria-hidden="true"></i>
+        </button>
+        <button class="btn btn-danger" onclick="deleteInfo(${index})">
+          <i class="fa fa-trash" aria-hidden="true"></i>
+        </button>
+        </td>
 
-
-
-carItem1.append(minicar1)
-minicar1.append(imagen1)
-cr1.append(carItem1)
-carItem1.append(cardbody)
-cardbody.append(text1)
-text1.append(titu1)
-text1.append(parr1)
-carItem1.append(boton1)
-boton1.append(ancla1)
-
-
-})
-buscadas.map((producto,index)=>{
-   
-    let carItem1=document.createElement("div")
-    if (index==0) {
-        carItem1.classList="carousel-item active  " 
-    }else{
-        carItem1.classList="carousel-item " 
-    }
-   let minicar1= document.createElement("div")
-   minicar1.classList="mini-carousel"
-   let imagen1=document.createElement("img")
-   imagen1.src=producto.imagen
-   imagen1.classList="d-block w-100"
-   imagen1.alt=producto.nombre
-   let cardbody=document.createElement("div")
-   cardbody.classList="card-body d-flex flex-column justify-content-between"
-let text1=document.createElement("div")
-text1.classList="text-center px-5 my-2"
-let titu1=document.createElement("h1")
-titu1.classList="badge text-bg-warning text-white fs-5"
-titu1.innerText="Las más buscadas"
-let parr1=document.createElement("p")
-parr1.classList="text-center"
-parr1.innerText="Estas son las notebook mas buscadas por la gente, con la mejor calidad y rendimiento."
-let boton1=document.createElement("div")
-boton1.classList="mt-auto text-center"
-let ancla1=document.createElement("a")
-ancla1.classList="btn btn-warning text-white fs-5 mb-3"
-ancla1.innerText="Más información"
-
-
-
-carItem1.append(minicar1)
-minicar1.append(imagen1)
-cr2.append(carItem1)
-carItem1.append(cardbody)
-cardbody.append(text1)
-text1.append(titu1)
-text1.append(parr1)
-carItem1.append(boton1)
-boton1.append(ancla1)
-
-
-})
+        </tr>`
+        userInfo.innerHTML+=createElement
+    })
 }
-cargarCar()
+const readInfo=(img,nombre,marca,precio,desc,stock)=>{
+document.querySelector("#showImgurl").value=img,
+document.querySelector("#showName").value=nombre,
+document.querySelector("#showBrand").value=marca,
+document.querySelector("#showPrice").value=precio,
+document.querySelector("#showDesc").value=desc,
+document.querySelector("#showStock").value=stock
+}
+const editInfo=(index,img,nombre,marca,precio,descr,cant)=>{
+isEdit=true
+editId=index
+imgurl.value=img
+productName.value=nombre
+brand.value=marca
+price.value=precio
+desc.value=descr
+stock.value=cant
+
+submitbtn.innerText="Actualizar"
+modalTitle.innerText="Actualizar el producto"
+}
+const deleteInfo=(index)=>{
+    if(confirm("Estas seguro que quieres eliminar el producto?")){
+        productos.splice(index,1)
+        localStorage.setItem("productos",JSON.stringify(productos))
+        showInfo()
+    }
+}
+showInfo()
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    const producto=new Producto(
+       productName.value,
+       price.value,
+       brand.value,
+       imgurl.value,
+       desc.value,
+       stock.value
+
+      
+    )
+    
+   
+   
+   
+ 
+     if(!isEdit){
+        productos.push(producto)
+     } else{
+        isEdit=false
+        productos[editId]=producto
+     }
+ 
+    localStorage.setItem("productos", JSON.stringify(productos))
+    submitbtn.innerText="Guardar"
+    modalTitle.innerHTML="Rellena el formulario"
+showInfo()
+    form.reset()
+    modal.style.display="none"
+    document.querySelector(".modal-backdrop").remove()
+})
+
+
+
+// let cr1 = document.querySelector(".cr1")
+// let cr2 = document.querySelector(".cr2")
+// const cargarCar= ()=>{
+// cr1.innerHTML=""
+// cr2.innerHTML=""
+// vendidas=productos.filter((producto)=>producto.stock<10)
+// buscadas=productos.filter((producto)=>producto.stock>10)
+// vendidas.map((producto,index)=>{
+   
+//     let carItem1=document.createElement("div")
+//     if (index==0) {
+//         carItem1.classList="carousel-item active  " 
+//     }else{
+//         carItem1.classList="carousel-item " 
+//     }
+//    let minicar1= document.createElement("div")
+//    minicar1.classList="mini-carousel"
+//    let imagen1=document.createElement("img")
+//    imagen1.src=producto.imagen
+//    imagen1.classList="d-block w-100"
+//    imagen1.alt=producto.nombre
+//    let cardbody=document.createElement("div")
+//    cardbody.classList="card-body d-flex flex-column justify-content-between"
+// let text1=document.createElement("div")
+// text1.classList="text-center px-5 my-2"
+// let titu1=document.createElement("h1")
+// titu1.classList="badge text-bg-warning text-white fs-5"
+// titu1.innerText="Las más vendidas"
+// let parr1=document.createElement("p")
+// parr1.classList="text-center"
+// parr1.innerText="Estas son las notebook más vendidas en el mercado, con los mejores componentes"
+// let boton1=document.createElement("div")
+// boton1.classList="mt-auto text-center"
+// let ancla1=document.createElement("a")
+// ancla1.classList="btn btn-warning text-white fs-5 mb-3"
+// ancla1.innerText="Más información"
+
+
+
+// carItem1.append(minicar1)
+// minicar1.append(imagen1)
+// cr1.append(carItem1)
+// carItem1.append(cardbody)
+// cardbody.append(text1)
+// text1.append(titu1)
+// text1.append(parr1)
+// carItem1.append(boton1)
+// boton1.append(ancla1)
+
+
+// })
+// buscadas.map((producto,index)=>{
+   
+//     let carItem1=document.createElement("div")
+//     if (index==0) {
+//         carItem1.classList="carousel-item active  " 
+//     }else{
+//         carItem1.classList="carousel-item " 
+//     }
+//    let minicar1= document.createElement("div")
+//    minicar1.classList="mini-carousel"
+//    let imagen1=document.createElement("img")
+//    imagen1.src=producto.imagen
+//    imagen1.classList="d-block w-100"
+//    imagen1.alt=producto.nombre
+//    let cardbody=document.createElement("div")
+//    cardbody.classList="card-body d-flex flex-column justify-content-between"
+// let text1=document.createElement("div")
+// text1.classList="text-center px-5 my-2"
+// let titu1=document.createElement("h1")
+// titu1.classList="badge text-bg-warning text-white fs-5"
+// titu1.innerText="Las más buscadas"
+// let parr1=document.createElement("p")
+// parr1.classList="text-center"
+// parr1.innerText="Estas son las notebook mas buscadas por la gente, con la mejor calidad y rendimiento."
+// let boton1=document.createElement("div")
+// boton1.classList="mt-auto text-center"
+// let ancla1=document.createElement("a")
+// ancla1.classList="btn btn-warning text-white fs-5 mb-3"
+// ancla1.innerText="Más información"
+
+
+
+// carItem1.append(minicar1)
+// minicar1.append(imagen1)
+// cr2.append(carItem1)
+// carItem1.append(cardbody)
+// cardbody.append(text1)
+// text1.append(titu1)
+// text1.append(parr1)
+// carItem1.append(boton1)
+// boton1.append(ancla1)
+
+
+// })
+// }
+// cargarCar()
