bd5f0bea3febb19f9a8c30361c2ae270327267ce
Franco Garcia <garciafranco_01@live.com>
2023-08-31 16:47:10 -0300

    separe la clase y la puse junto al array para exportar,los carouseles de index ya funcionan

--- js/admin.js
+++ js/admin.js
@@ -1,22 +1,4 @@
-class Producto {
-    constructor(nombre,precio,marca,imagen,descripcion,stock){
-        this.id = Producto.getNextId()
-        this.nombre=nombre
-        this.precio=precio
-        this.marca=marca
-        this.imagen=imagen
-        this.descripcion=descripcion
-        this.stock=stock
-    }
-    static getNextId() {
-        if (!this.nextId) {
-          this.nextId = 1;
-        }
-        return this.nextId++;
-      }
-}
-
-const productos=JSON.parse(localStorage.getItem("productos")) ||[]
+import {productos,Producto} from "./clases.js"
 
 
 let form= document.querySelector("#myForm"),
@@ -47,7 +29,7 @@ const showInfo= ()=>{
         <button class="btn btn-success"  data-bs-toggle="modal" data-bs-target="#readData" onclick="readInfo('${producto.imagen}', '${producto.nombre}', '${producto.marca}', '${producto.precio}', '${producto.descripcion}', '${producto.stock}')">
           <i class="fa fa-eye" aria-hidden="true"></i>
         </button>
-        <button class="btn btn-primary" onclick="editInfo(${index},'${producto.imagen}','${producto.nombre}','${producto.marca}','${producto.precio}','${producto.descripcion}','${producto.stock}')" data-bs-toggle="modal" data-bs-target="#userForm">
+        <button class="btn btn-primary" onclick="editInfo(${index},'${producto.imagen}','${producto.nombre}','${producto.marca}','${producto.precio}','${producto.descripcion}','${producto.stock}')" data-bs-toggle="modal" data-bs-target="#userForm" id="edit">
           <i class="fa fa-pencil" aria-hidden="true"></i>
         </button>
         <button class="btn btn-danger" onclick="deleteInfo(${index})">
@@ -59,7 +41,7 @@ const showInfo= ()=>{
         userInfo.innerHTML+=createElement
     })
 }
-const readInfo=(img,nombre,marca,precio,desc,stock)=>{
+window.readInfo=(img,nombre,marca,precio,desc,stock)=>{
 document.querySelector("#showImgurl").value=img,
 document.querySelector("#showName").value=nombre,
 document.querySelector("#showBrand").value=marca,
@@ -68,7 +50,7 @@ document.querySelector("#showDesc").value=desc,
 document.querySelector("#showStock").value=stock
 }
 
-const editInfo=(index,img,nombre,marca,precio,descr,cant)=>{
+window.editInfo=(index,img,nombre,marca,precio,descr,cant)=>{
 isEdit=true
 editId=index
 imgurl.value=img
@@ -82,7 +64,7 @@ submitbtn.innerText="Actualizar"
 modalTitle.innerText="Actualizar el producto"
 }
 
-const deleteInfo=(index)=>{
+window.deleteInfo=(index)=>{
     if(confirm("Estas seguro que quieres eliminar el producto?")){
         productos.splice(index,1)
         localStorage.setItem("productos",JSON.stringify(productos))
@@ -129,106 +111,3 @@ const newProduct=()=>{
 }
 let btnNew=document.getElementById("btnNew")
 btnNew.addEventListener("click",newProduct)
-
-productosEx=productos
-
-
-
-
-// let cr1 = document.querySelector(".cr1")
-// let cr2 = document.querySelector(".cr2")
-// const cargarCar= ()=>{
-// cr1.innerHTML=""
-// cr2.innerHTML=""
-// vendidas=productos.filter((producto)=>producto.stock<10)
-// buscadas=productos.filter((producto)=>producto.stock>10)
-// vendidas.map((producto,index)=>{
-   
-//     let carItem1=document.createElement("div")
-//     if (index==0) {
-//         carItem1.classList="carousel-item active  " 
-//     }else{
-//         carItem1.classList="carousel-item " 
-//     }
-//    let minicar1= document.createElement("div")
-//    minicar1.classList="mini-carousel"
-//    let imagen1=document.createElement("img")
-//    imagen1.src=producto.imagen
-//    imagen1.classList="d-block w-100"
-//    imagen1.alt=producto.nombre
-//    let cardbody=document.createElement("div")
-//    cardbody.classList="card-body d-flex flex-column justify-content-between"
-// let text1=document.createElement("div")
-// text1.classList="text-center px-5 my-2"
-// let titu1=document.createElement("h1")
-// titu1.classList="badge text-bg-warning text-white fs-5"
-// titu1.innerText="Las más vendidas"
-// let parr1=document.createElement("p")
-// parr1.classList="text-center"
-// parr1.innerText="Estas son las notebook más vendidas en el mercado, con los mejores componentes"
-// let boton1=document.createElement("div")
-// boton1.classList="mt-auto text-center"
-// let ancla1=document.createElement("a")
-// ancla1.classList="btn btn-warning text-white fs-5 mb-3"
-// ancla1.innerText="Más información"
-
-
-
-// carItem1.append(minicar1)
-// minicar1.append(imagen1)
-// cr1.append(carItem1)
-// carItem1.append(cardbody)
-// cardbody.append(text1)
-// text1.append(titu1)
-// text1.append(parr1)
-// carItem1.append(boton1)
-// boton1.append(ancla1)
-
-
-// })
-// buscadas.map((producto,index)=>{
-   
-//     let carItem1=document.createElement("div")
-//     if (index==0) {
-//         carItem1.classList="carousel-item active  " 
-//     }else{
-//         carItem1.classList="carousel-item " 
-//     }
-//    let minicar1= document.createElement("div")
-//    minicar1.classList="mini-carousel"
-//    let imagen1=document.createElement("img")
-//    imagen1.src=producto.imagen
-//    imagen1.classList="d-block w-100"
-//    imagen1.alt=producto.nombre
-//    let cardbody=document.createElement("div")
-//    cardbody.classList="card-body d-flex flex-column justify-content-between"
-// let text1=document.createElement("div")
-// text1.classList="text-center px-5 my-2"
-// let titu1=document.createElement("h1")
-// titu1.classList="badge text-bg-warning text-white fs-5"
-// titu1.innerText="Las más buscadas"
-// let parr1=document.createElement("p")
-// parr1.classList="text-center"
-// parr1.innerText="Estas son las notebook mas buscadas por la gente, con la mejor calidad y rendimiento."
-// let boton1=document.createElement("div")
-// boton1.classList="mt-auto text-center"
-// let ancla1=document.createElement("a")
-// ancla1.classList="btn btn-warning text-white fs-5 mb-3"
-// ancla1.innerText="Más información"
-
-
-
-// carItem1.append(minicar1)
-// minicar1.append(imagen1)
-// cr2.append(carItem1)
-// carItem1.append(cardbody)
-// cardbody.append(text1)
-// text1.append(titu1)
-// text1.append(parr1)
-// carItem1.append(boton1)
-// boton1.append(ancla1)
-
-
-// })
-// }
-// cargarCar()
---
+++ js/clases.js
@@ -0,0 +1,125 @@
+class Producto {
+    constructor(nombre,precio,marca,imagen,descripcion,stock){
+        this.id = Producto.getNextId()
+        this.nombre=nombre
+        this.precio=precio
+        this.marca=marca
+        this.imagen=imagen
+        this.descripcion=descripcion
+        this.stock=stock
+    }
+    static getNextId() {
+        if (!this.nextId) {
+          this.nextId = 1;
+        }
+        return this.nextId++;
+      }
+}
+
+const productos=JSON.parse(localStorage.getItem("productos")) ||[]
+export {Producto,productos}
+
+
+
+
+
+
+
+
+// let cr1 = document.querySelector(".cr1")
+// let cr2 = document.querySelector(".cr2")
+// const cargarCar= ()=>{
+// cr1.innerHTML=""
+// cr2.innerHTML=""
+// vendidas=productos.filter((producto)=>producto.stock<10)
+// buscadas=productos.filter((producto)=>producto.stock>10)
+// vendidas.map((producto,index)=>{
+   
+//     let carItem1=document.createElement("div")
+//     if (index==0) {
+//         carItem1.classList="carousel-item active  " 
+//     }else{
+//         carItem1.classList="carousel-item " 
+//     }
+//    let minicar1= document.createElement("div")
+//    minicar1.classList="mini-carousel"
+//    let imagen1=document.createElement("img")
+//    imagen1.src=producto.imagen
+//    imagen1.classList="d-block w-100"
+//    imagen1.alt=producto.nombre
+//    let cardbody=document.createElement("div")
+//    cardbody.classList="card-body d-flex flex-column justify-content-between"
+// let text1=document.createElement("div")
+// text1.classList="text-center px-5 my-2"
+// let titu1=document.createElement("h1")
+// titu1.classList="badge text-bg-warning text-white fs-5"
+// titu1.innerText="Las más vendidas"
+// let parr1=document.createElement("p")
+// parr1.classList="text-center"
+// parr1.innerText="Estas son las notebook más vendidas en el mercado, con los mejores componentes"
+// let boton1=document.createElement("div")
+// boton1.classList="mt-auto text-center"
+// let ancla1=document.createElement("a")
+// ancla1.classList="btn btn-warning text-white fs-5 mb-3"
+// ancla1.innerText="Más información"
+
+
+
+// carItem1.append(minicar1)
+// minicar1.append(imagen1)
+// cr1.append(carItem1)
+// carItem1.append(cardbody)
+// cardbody.append(text1)
+// text1.append(titu1)
+// text1.append(parr1)
+// carItem1.append(boton1)
+// boton1.append(ancla1)
+
+
+// })
+// buscadas.map((producto,index)=>{
+   
+//     let carItem1=document.createElement("div")
+//     if (index==0) {
+//         carItem1.classList="carousel-item active  " 
+//     }else{
+//         carItem1.classList="carousel-item " 
+//     }
+//    let minicar1= document.createElement("div")
+//    minicar1.classList="mini-carousel"
+//    let imagen1=document.createElement("img")
+//    imagen1.src=producto.imagen
+//    imagen1.classList="d-block w-100"
+//    imagen1.alt=producto.nombre
+//    let cardbody=document.createElement("div")
+//    cardbody.classList="card-body d-flex flex-column justify-content-between"
+// let text1=document.createElement("div")
+// text1.classList="text-center px-5 my-2"
+// let titu1=document.createElement("h1")
+// titu1.classList="badge text-bg-warning text-white fs-5"
+// titu1.innerText="Las más buscadas"
+// let parr1=document.createElement("p")
+// parr1.classList="text-center"
+// parr1.innerText="Estas son las notebook mas buscadas por la gente, con la mejor calidad y rendimiento."
+// let boton1=document.createElement("div")
+// boton1.classList="mt-auto text-center"
+// let ancla1=document.createElement("a")
+// ancla1.classList="btn btn-warning text-white fs-5 mb-3"
+// ancla1.innerText="Más información"
+
+
+
+// carItem1.append(minicar1)
+// minicar1.append(imagen1)
+// cr2.append(carItem1)
+// carItem1.append(cardbody)
+// cardbody.append(text1)
+// text1.append(titu1)
+// text1.append(parr1)
+// carItem1.append(boton1)
+// boton1.append(ancla1)
+
+
+// })
+// }
+// cargarCar()
